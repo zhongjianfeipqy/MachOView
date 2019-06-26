eea7bd555b02b3a32839e130f6d5f3fa5eb5d728
JF Z <a378789308@gmail.com>
2019-06-26 11:58:15 +0800

    优化注释

--- ReadWrite.mm
+++ ReadWrite.mm
@@ -163,9 +163,6 @@
     // 转成uint8_t字符串，如："\x11b"
     uint8_t *cstr = (uint8_t *)[fileData bytes] + range.location;
     
-    // 通过utf-16转成字符串，后面会带\0\0
-    NSString *temp = [[NSString alloc] initWithCString:(char *)cstr encoding:NSUTF16StringEncoding];
-    
     /*
      NSNonLossyASCIIStringEncoding：能读取到结束符\0
      NSISO2022JPStringEncoding：能读取到结束符\0
@@ -177,37 +174,39 @@
     NSString *endStr = [[NSString alloc] initWithBytes:cstr length:readByteNum encoding:NSNonLossyASCIIStringEncoding];
     // 优化方案：修改cstr，让cstr每次后移1个字节
     // 为什么是两个\0\0，因为在ustring中，存储内存中，都是以两个字节存放一个数据，那么字符串的结尾'\0'是以一个字节往高位（二进制中的高8位）存储，但是会分配两个字节
-//    NSString *ctns = @"\0\0";
-//    ![endStr hasSuffix:ctns]
     int zeroCount = 0;
     BOOL isCycle = YES;
     while (isCycle) { // 继续往下读取
         endStr = [[NSString alloc] initWithBytes:cstr length:++readByteNum encoding:NSISO2022JPStringEncoding];
         if ([endStr hasSuffix:@"\0"]) {
             zeroCount++;
+            // 如果下一个字节结尾不是"\0"，那么判断是否已经读取完了
             NSString *nextStr = [[NSString alloc] initWithBytes:cstr length:readByteNum + 1 encoding:NSISO2022JPStringEncoding];
             if ([nextStr hasSuffix:@"\0"]) {
                 
             } else if (zeroCount >= 2) {
                 isCycle = NO;
+                /*
+                    ustring中会用两个字节空间存放一个数据（英文和中文都是）
+                    英文只会占用一个字节
+                    那么读取的字节数必然是2的倍数
+                 */
                 if (readByteNum % 2 != 0) {
                     readByteNum--;
                 }
-            }
-            else {
+            } else {
                 zeroCount = 0;
             }
         }
     }
     endStr = [[NSString alloc] initWithBytes:cstr length:readByteNum encoding:NSISO2022JPStringEncoding];
-
+    // 读取到的字符串
     NSString *readStr = [[NSString alloc] initWithBytes:cstr length:readByteNum encoding:NSUTF16LittleEndianStringEncoding];
-//    NSString *readStr = [[NSString alloc] initWithBytes:cstr length:readByteNum encoding:NSMacOSRomanStringEncoding];
-
-    temp = readStr;
+    // 记录当前读取的位置
     range.length = readByteNum;
+    // 16进制数据读写
     if (lastReadHex) *lastReadHex = [self getHexStr:range];
-    return [self replaceEscapeCharsInString:temp];
+    return [self replaceEscapeCharsInString:readStr];
 }
 
 - (BOOL)isChineseWithStr:(NSString *)str
